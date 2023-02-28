fc3ccf550a66d00acb2690e6ae84c5d9d677272b
Amritpal9854343 <126471012+Amritpal9854343@users.noreply.github.com>
2023-02-28 16:12:44 +0530

    Add files via upload

---
+++ flask_server.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import request
+import numpy as np
+import pickle
+
+
+with open('./backend/Models/banglore_home_prices_model.pickle', 'rb') as p:
+    pipe = pickle.load(p)    
+
+app = Flask(__name__)
+
+@app.route('/pricePrediction', methods=['POST'])
+def index():
+    temp=np.zeros(244)
+    temp[int(request.form["location"])]=1
+    temp[0] = request.form["sqft"]
+    temp[1] = request.form["bath"]
+    temp[2] = request.form["bhk"]
+    price = pipe.predict([temp])[0]
+    price=price.item()
+    price=round(price,2)
+    priceDict = {"price" : price}
+    return priceDict
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
